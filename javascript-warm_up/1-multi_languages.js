85aa5c8028a7ac0a132cd2cd5cb938144923ac2b
abdureufshifa <shifabdureuf@gmail.com>
2023-09-06 14:07:43 +0300

    javascript-warm_up

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+const firstLine = 'C is fun';
+const secondLine = 'Python is coo';
+const ThreedLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(ThreedLine);
---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,4 @@
+const arrayLists = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+for (let i = 0; i <= arrayLists.length; i++) {
+  console.log(arrayLists[i]);
+}
