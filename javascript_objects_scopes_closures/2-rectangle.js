afd8fb42a43afb63cba406ceac16f37c6bd6c1e0
abdureufshifa <shifabdureuf@gmail.com>
2023-09-07 17:30:05 +0300

    javascript_objects_scopes_closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,15 +2,15 @@
 class Rectangle {
     constructor(w, h) {
       if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-        return {};
+        return 'Rectangle {}';
       }
       this.width = w;
       this.height = h;
     }
   
-    toString() {
-      return 'Rectangle {}';
-    }
+    // toString() {
+    //   return 'Rectangle {}';
+    // }
   }
   
   module.exports = Rectangle;
