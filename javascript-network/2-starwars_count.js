6f89e68dbb2d00dfd588c7d1c409eeb1c496a315
abdureufshifa <shifabdureuf@gmail.com>
2023-09-11 17:38:54 +0300

    p2-js_network

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,36 +1,18 @@
 const request = require('request');
 
-const apiUrl = process.argv[2];
-const characterId = '18';
-let count = 0;
+const urlProcess = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/people/18/`;
 
-request.get(apiUrl, (error, response, body) => {
+request.get(urlProcess, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error.message}`);
   } else if (response.statusCode !== 200) {
     console.error(`Error: Received status code ${response.statusCode}`);
   } else {
     const filmsData = JSON.parse(body);
-    const fetchCharacterData = (characterUrl) => {
-      request.get(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-        } else if (response.statusCode !== 200) {
-          console.error(`Error: Received status code ${response.statusCode}`);
-        } else {
-          const characterData = JSON.parse(body);
-          if (characterData && characterData.name === 'Wedge Antilles') {
-            count++;
-          }
-        }
-        if (filmsData.next) {
-          fetchCharacterData(filmsData.next);
-        } else {
-          console.log(count);
-        }
-      });
-    };
-
-    fetchCharacterData(filmsData.results[0].characters[0]);
+    const wedgeAntillesFilms = filmsData.results.filter((film) => {
+      return film.characters.includes(url);
+    });
+    console.log(wedgeAntillesFilms.length);
   }
 });
