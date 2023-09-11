b81b14ccba3eb11b3136545a49d5f01b576f8d2b
abdureufshifa <shifabdureuf@gmail.com>
2023-09-11 18:11:42 +0300

    p3-js_network

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -2,6 +2,10 @@ const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
 const filePath = process.argv[3];
 
 request.get(url, (error, response, body) => {
