a3b6600c9d8668c587d1983c08094c7850af8ea4
abdureufshifa <shifabdureuf@gmail.com>
2023-09-12 15:49:10 +0300

    p2-js_network

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -2,7 +2,6 @@ const request = require('request');
 
 const urlProcess = process.argv[2];
 
-
 request.get(urlProcess, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error.message}`);
@@ -11,8 +10,38 @@ request.get(urlProcess, (error, response, body) => {
   } else {
     const filmsData = JSON.parse(body);
     const wedgeAntillesFilms = filmsData.results.filter((film) => {
-      return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/18`);
+      return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
     });
-    console.log(wedgeAntillesFilms.length);
+
+    // Fetch additional pages if available
+    if (filmsData.next) {
+      fetchAdditionalPages(filmsData.next, wedgeAntillesFilms);
+    } else {
+      console.log(wedgeAntillesFilms.length);
+    }
   }
 });
+
+// Function to fetch additional pages of film data
+function fetchAdditionalPages(url, films) {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+    } else if (response.statusCode !== 200) {
+      console.error(`Error: Received status code ${response.statusCode}`);
+    } else {
+      const filmsData = JSON.parse(body);
+      const wedgeAntillesFilms = filmsData.results.filter((film) => {
+        return film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/');
+      });
+      films.push(...wedgeAntillesFilms);
+
+      // Fetch additional pages if available
+      if (filmsData.next) {
+        fetchAdditionalPages(filmsData.next, films);
+      } else {
+        console.log(films.length);
+      }
+    }
+  });
+}
