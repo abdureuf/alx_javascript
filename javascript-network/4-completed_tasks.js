4e6dd7cd06c909f147b7a5b18e91c10e1e3ccfca
abdureufshifa <shifabdureuf@gmail.com>
2023-09-12 16:48:10 +0300

    p4-js_network

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,22 +1,27 @@
 const request = require('request');
-const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
+const apiUrl = process.argv[2];
 
-request.get(url, (error, response, body) => {
+request.get(apiUrl, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error.message}`);
   } else if (response.statusCode !== 200) {
     console.error(`Error: Received status code ${response.statusCode}`);
   } else {
-    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-      if (err) {
-        console.error(`Error writing file: ${err}`);
-      }  
-    //   else {
-    //     console.log(`File "${filePath}" created successfully.`);
-    //   }
+    const todos = JSON.parse(body);
+
+    const completedTasksByUser = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
     });
+
+    console.log(completedTasksByUser);
   }
 });
