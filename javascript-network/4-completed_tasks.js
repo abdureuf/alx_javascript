31964110c4ca329e6f8f94e832bf0abb738d02ff
abdureufshifa <shifabdureuf@gmail.com>
2023-09-12 16:40:53 +0300

    p4-js_network

---
+++ javascript-network/4-completed_tasks.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: Received status code ${response.statusCode}`);
+  } else {
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+      if (err) {
+        console.error(`Error writing file: ${err}`);
+      }  
+    //   else {
+    //     console.log(`File "${filePath}" created successfully.`);
+    //   }
+    });
+  }
+});
